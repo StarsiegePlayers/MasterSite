f3baca058d1cc5fbe3fc6429de9df248d4def435
Amrit Panesar <me@amrit.pw>
2020-08-06 01:34:57 -0700

    Break out config.php

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+const DEBUG = false;
+set_time_limit (60);
+
+$masters = [
+    [
+        "server" => "master1.starsiege.io",
+        "port" => 29000,
+    ],
+    [
+        "server" => "master2.starsiege.pw",
+        "port" => 29000,
+    ],
+    [
+        "server" => "master3.starsiege.io",
+        "port" => 29000,
+    ],
+    [
+        "server" => "starsiege1.no-ip.org",
+        "port" => 29000,
+    ],
+/*
+    [
+        "server"    => "starsiege.noip.us",
+        "port"      => 29000,
+    ],
+    [
+        "server"    => "154.0.175.219",
+        "port"      => 29000,
+    ],
+*/
+];
--- query.php
+++ query.php
@@ -1,46 +1,24 @@
 <?php
 require_once("query/master.php");
 require_once("query/game.php");
-// set_time_limit (60);
-$masters = [
-    [
-        "server" => "master1.starsiege.io",
-        "port" => 29000,
-    ],
-    [
-        "server" => "master2.starsiege.pw",
-        "port" => 29000,
-    ],
-    [
-        "server" => "master3.starsiege.io",
-        "port" => 29000,
-    ],
-    [
-        "server" => "starsiege1.no-ip.org",
-        "port" => 29000,
-    ],/*
-    [
-        "server"    => "starsiege.noip.us",
-        "port"      => 29000,
-    ], 
-    [
-        "server"    => "154.0.175.219",
-        "port"      => 29000,
-    ], */
-];
+require_once("config.php");
 
 $masterData = [];
 $masterServer = [];
 
 foreach ($masters as $master) {
-    // printf("probing {$master['server']}");
+    if (DEBUG)
+        printf("probing {$master['server']}");
+
     $query = queryMasterServer($master['server'], $master['port']);
     $masterServer[] = $query;
     if (count($query['servers']) <= 0) {
         printf("Error server count was 0");
         exit;
     }
-    // printf("Acquired %d servers in %s ms\n", count($query['servers']), $query['ping']);
+
+    if (DEBUG)
+        printf("Acquired %d servers in %s ms\n", count($query['servers']), $query['ping']);
 
     foreach ($query['servers'] as $server) {
         $uniqueID = sprintf("%s:%s", $server["host"], $server['port']);
@@ -59,5 +37,7 @@ foreach ($masterData as $server) {
 }
 $end = microtime(true);
 
-// printf("Queried %d servers in %s ms\n", count($gameServers), $end - $start);
-// print_r($gameServers);
+if (DEBUG) {
+    printf("Queried %d servers in %s ms\n", count($gameServers), $end - $start);
+    print_r($gameServers);
+}
